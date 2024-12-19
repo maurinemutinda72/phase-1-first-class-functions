438bc5f74bcaaa775ef4ba1e00569a6f321f39ed
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 11:42:12 +0300

    completed task

---
+++ index.js
@@ -0,0 +1,8 @@
+// Function 1: Define using an arrow function
+const divide = () => 2000 / 100;
+
+// Function 2: Define using an arrow function with a single parameter
+const square = x => x * x;
+
+// Function 3: Define using an arrow function with two parameters
+const add = (a, b) => a + b;
